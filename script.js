703dfab2bc150eac23a7242c540361fade643e36
EVGTStudio <tierralinda.atitlan@gmail.com>
2026-07-16 20:50:02 -0600

    2.0 Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,62 @@
-window.addEventListener("load", () => {
+const letters = document.querySelectorAll("#letters span");
+const claim = document.getElementById("claim");
 
-    const hero = document.querySelector(".hero");
+letters.forEach((letter, index) => {
 
-    hero.animate(
-        [
+    setTimeout(() => {
+
+        letter.animate(
+
+            [
+                {
+                    opacity:0,
+                    transform:"translateY(30px)"
+                },
+
+                {
+                    opacity:1,
+                    transform:"translateY(0px)"
+                }
+
+            ],
+
             {
-                opacity: 0,
-                transform: "translateY(30px)"
-            },
-            {
-                opacity: 1,
-                transform: "translateY(0)"
+
+                duration:700,
+                easing:"ease-out",
+                fill:"forwards"
+
             }
-        ],
-        {
-            duration: 1800,
-            easing: "ease-out",
-            fill: "forwards"
-        }
-    );
+
+        );
+
+    }, index * 550);
 
 });
+
+setTimeout(() => {
+
+    claim.animate(
+
+        [
+
+            {
+                opacity:0
+            },
+
+            {
+                opacity:1
+            }
+
+        ],
+
+        {
+
+            duration:1500,
+            fill:"forwards"
+
+        }
+
+    );
+
+}, 3300);
